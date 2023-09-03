2352ff10a83fd1ac4e190f50f9a26ecc2c65a88d
melvinchia3636 <melvinchia623600@gmail.com>
2023-09-03 08:12:14 +0800

    cool

--- client/src/lib/Blog.svelte
+++ client/src/lib/Blog.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-  import { Link } from "svelte-navigator";
+  import { Link, navigate } from "svelte-navigator";
   import { fade } from "svelte/transition";
   import moment from "moment";
 
@@ -16,9 +16,21 @@
 
   let showContent = false;
 
+  const setShowContent = (value) => {
+    showContent = value;
+  };
+
   setTimeout(() => {
     showContent = true;
   }, 500);
+
+  let showBlogContent = false;
+
+  $: if (blogData.length) {
+    setTimeout(() => {
+      showBlogContent = true;
+    }, 500);
+  }
 </script>
 
 <main class="w-full h-full flex flex-col items-center" id="swup">
@@ -28,51 +40,60 @@
       in:fade
       out:fade
     >
-      <Link
-        to="/"
-        class="tracking-[0.2em] uppercase font-medium text-[#FFAA4C] -ml-4 flex items-center gap-2"
-      >
-        <Icon icon="uil:arrow-left" class="w-5 h-5 -mt-[1px]" />
-        go back
-      </Link>
       <h1
         class="font-medium text-center flex tracking-[0.2em] px-12 uppercase text-4xl mt-6 mb-20 relative after:content-[''] after:absolute after:w-24 after:border-b-2 after:border-[#FFAA4C] after:-bottom-4 after:left-1/2 after:-translate-x-[55%]"
       >
         my life journey
       </h1>
-      <div class="flex w-full px-8 sm:!w-4/5 flex-col gap-8 text-left">
-        {#if blogData.length}
-          {#each blogData as blog}
-            <div
-              class="flex flex-col bg-slate-100 bg-opacity-5 rounded-md w-full h-full p-6"
-            >
-              <div class="flex flex-wrap gap-2 mb-2 text-sm font-medium">
-                {moment(blog.date).format("Do MMM YYYY")}
-              </div>
-              <h2 class="text-2xl font-medium text-[#FFAA4C]">
-                {blog.title.rendered}
-              </h2>
-              <p
-                class="text-sm mt-2 description tracking-[0.08em] text-slate-400"
+      {#if blogData.length}
+        {#if showBlogContent}
+          <div
+            in:fade
+            out:fade
+            class="flex w-full px-8 sm:!w-4/5 flex-col gap-8 text-left"
+          >
+            {#each blogData as blog}
+              <div
+                class="flex flex-col bg-slate-100 bg-opacity-5 rounded-md w-full h-full p-6"
               >
-                {@html blog.excerpt.rendered || "No description"}
-              </p>
-              <Link to={`/blog/${blog.id}`} class="mt-auto w-full sm:!w-min block">
-                <button
-                  class="tracking-[0.2em] whitespace-nowrap w-full sm:w-!auto mt-8 flex items-center font-semibold text-sm rounded-sm justify-center bg-[#FFAA4C] px-8 text-neutral-800 pb-[1.2rem] pt-5 uppercase gap-2"
+                <div class="flex flex-wrap gap-2 mb-2 text-sm font-medium">
+                  {moment(blog.date).format("Do MMM YYYY")}
+                </div>
+                <h2 class="text-2xl font-medium text-[#FFAA4C]">
+                  {blog.title.rendered}
+                </h2>
+                <p
+                  class="text-sm mt-2 description tracking-[0.08em] text-slate-400"
                 >
-                  Read More
-                  <Icon icon="uil:arrow-right" class="w-5 h-5 flex-shrink-0" />
-                </button>
-              </Link>
-            </div>
-          {/each}
-        {:else}
-          <div class="flex px-8 w-full flex-col gap-20 text-left items-center">
-            <Icon icon="svg-spinners:180-ring" class="w-8 h-8" />
+                  {@html blog.excerpt.rendered || "No description"}
+                </p>
+                <Link
+                  to={`/blog/${blog.id}`}
+                  class="mt-auto w-full sm:!w-min block"
+                >
+                  <button
+                    class="tracking-[0.2em] whitespace-nowrap w-full sm:w-!auto mt-8 flex items-center font-semibold text-sm rounded-sm justify-center bg-[#FFAA4C] px-8 text-neutral-800 pb-[1.2rem] pt-5 uppercase gap-2"
+                  >
+                    Read More
+                    <Icon
+                      icon="uil:arrow-right"
+                      class="w-5 h-5 flex-shrink-0"
+                    />
+                  </button>
+                </Link>
+              </div>
+            {/each}
           </div>
         {/if}
-      </div>
+      {:else}
+        <div
+          in:fade
+          out:fade
+          class="flex px-8 w-full flex-col gap-20 text-left items-center"
+        >
+          <Icon icon="svg-spinners:180-ring" class="w-8 h-8" />
+        </div>
+      {/if}
     </div>
   {/if}
 </main>
--- client/src/lib/BlogContent.svelte
+++ client/src/lib/BlogContent.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
-  import { Link, useParams } from "svelte-navigator";
+  import { Link, useParams, navigate } from "svelte-navigator";
   import { fade } from "svelte/transition";
 
   const params = useParams();
@@ -10,7 +10,6 @@
   }, 500);
 
   let blogData = null;
-
 
   fetch(
     `https://blog.thecodeblog.net/wp-json/wp/v2/posts/${$params.id}?_fields=content,title,date`
@@ -22,10 +21,21 @@
 
   let showContent = false;
 
+  const setShowContent = (value) => {
+    showContent = value;
+  };
+
   setTimeout(() => {
     showContent = true;
   }, 500);
 
+  let showBlogContent = false;
+
+  $: if (blogData) {
+    setTimeout(() => {
+      showBlogContent = true;
+    }, 500);
+  }
 </script>
 
 <main class="w-full h-full flex flex-col items-center" id="swup">
@@ -35,67 +45,90 @@
       in:fade
       out:fade
     >
-      <Link
-        to="/"
+      <button
+      on:click={() => {
+        setShowContent(false);
+        setTimeout(() => {
+          navigate("/blog");
+        }, 500);
+      }}
         class="tracking-[0.2em] uppercase font-medium text-[#FFAA4C] -ml-4 flex items-center gap-2"
       >
         <Icon icon="uil:arrow-left" class="w-5 h-5 -mt-[1px]" />
         go back
-      </Link>
+      </button>
       {#if blogData}
-      <h1
-        class="font-medium text-center w-4/5 flex tracking-[0.2em] uppercase text-3xl mt-6 mb-20 relative after:content-[''] after:absolute after:w-24 after:border-b-2 after:border-[#FFAA4C] after:-bottom-4 after:left-1/2 after:-translate-x-[55%]"
-      >
-        {blogData.title.rendered}
-      </h1>
-      <div id="content" class="w-4/5 flex flex-col gap-8 items-center">
-        {@html blogData.content.rendered}
-      </div>
+        {#if showBlogContent}
+          <h1
+            in:fade
+            out:fade
+            class="font-medium text-center w-4/5 flex tracking-[0.2em] uppercase text-3xl mt-6 mb-20 relative after:content-[''] after:absolute after:w-24 after:border-b-2 after:border-[#FFAA4C] after:-bottom-4 after:left-1/2 after:-translate-x-[55%]"
+          >
+            {blogData.title.rendered}
+          </h1>
+          <div
+            in:fade
+            out:fade
+            id="content"
+            class="w-4/5 flex flex-col gap-8 items-center"
+          >
+            {@html blogData.content.rendered}
+          </div>
+        {/if}
+      {:else}
+        <div
+          in:fade
+          out:fade
+          class="flex px-8 py-8 w-full flex-col gap-20 text-left items-center"
+        >
+          <Icon icon="svg-spinners:180-ring" class="w-8 h-8" />
+        </div>
       {/if}
     </div>
   {/if}
 </main>
 
 <style>
-    :global(#content p) {
-        @apply w-full text-lg;
-    }
-    :global(#content figcaption) {
-        @apply w-full text-center text-slate-500;
-    }
+  :global(#content p) {
+    @apply w-full text-lg;
+  }
+  :global(#content figcaption) {
+    @apply w-full text-center text-slate-500;
+  }
 
-    :global(#content blockquote) {
-        @apply border-l-2 border-slate-100 w-full p-4
-    }
+  :global(#content blockquote) {
+    @apply border-l-2 border-slate-100 w-full p-4;
+  }
 
-    :global(#content h2) {
-        @apply text-3xl font-medium text-[#FFAA4C] mt-8 -mb-2;
-    }
+  :global(#content h2) {
+    @apply text-3xl font-medium text-[#FFAA4C] mt-8 -mb-2;
+  }
 
-    :global(#content figure) {
-        @apply w-full flex flex-col items-center gap-4;
-    }
+  :global(#content figure) {
+    @apply w-full flex flex-col items-center gap-4;
+  }
 
-    :global(#content table) {
-        @apply w-full border-collapse border border-slate-100;
-    }
+  :global(#content table) {
+    @apply w-full border-collapse border border-slate-100;
+  }
 
-    :global(#content table tr) {
-        @apply border-b border-slate-100;
-    }
+  :global(#content table tr) {
+    @apply border-b border-slate-100;
+  }
 
-    :global(#content table td) {
-        @apply border-r border-slate-100 p-3;
-    }
+  :global(#content table td) {
+    @apply border-r border-slate-100 p-3;
+  }
 
-    :global(#content figure.wp-block-gallery) {
-        @apply w-full flex flex-row flex-wrap gap-4 justify-center;
-    }
+  :global(#content figure.wp-block-gallery) {
+    @apply w-full flex flex-row flex-wrap gap-4 justify-center;
+  }
 
-    :global(#content figure.wp-block-gallery figure.wp-block-image) {
-        @apply w-96;
-    }
-    :global(#content img) {
-        @apply w-96 !h-auto object-cover;
-    }
+  :global(#content figure.wp-block-gallery figure.wp-block-image) {
+    @apply w-96;
+  }
+  :global(#content img) {
+    @apply object-cover;
+    height: auto !important;
+  }
 </style>
