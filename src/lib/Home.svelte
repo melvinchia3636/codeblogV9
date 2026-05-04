441b39414873374aedf99e0249ab83f321a36918
melvinchia3636 <melvinchia623600@gmail.com>
2026-05-04 23:38:06 +0800

    remove hidden md:block from copyright footer to show on all screens

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -85,7 +85,7 @@
   <div
     in:fade
     out:fade
-    class="fixed pointer-events-none bottom-12 w-full text-center px-8 left-1/2 -translate-x-1/2 text-zinc-100 z-[9999] uppercase text-xs font-light hidden md:block tracking-[0.2em]"
+    class="fixed pointer-events-none bottom-12 w-full text-center px-8 left-1/2 -translate-x-1/2 text-zinc-100 z-[9999] uppercase text-xs font-light tracking-[0.2em]"
   >
     Copyright &copy; 2020 - {new Date().getFullYear()} Melvin Chia; All rights reserved;
   </div>
