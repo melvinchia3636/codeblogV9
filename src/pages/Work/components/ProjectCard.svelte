16e75611d2fe172eef046b6892e37649537133e8
melvinchia3636 <melvinchia623600@gmail.com>
2026-05-05 07:33:32 +0800

    refactor Work into folder with sub-components, extract ProjectCard

---
+++ src/pages/Work/components/ProjectCard.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Icon from "@iconify/svelte";
+  import Button from "../../components/Button.svelte";
+
+  export let name = "";
+  export let desc = "";
+  export let repo = "";
+  export let url = "";
+  export let image = "";
+</script>
+
+<div class="h-full saos">
+  <div
+    class="flex font-light gap-8 flex-col h-full bg-zinc-100 p-4 bg-opacity-5"
+  >
+    {#if image}
+      <img
+        src={image}
+        class="object-contain border border-neutral-700 rounded-sm w-full {repo ? 'h-48' : ''}"
+        alt=""
+      />
+    {/if}
+    <div class="flex flex-col justify-between h-full">
+      <div class="flex flex-col gap-4">
+        <div
+          class="uppercase tracking-[0.2em] font-medium text-lg text-[#FFAA4C]"
+        >
+          {name}
+        </div>
+        <p class="!text-xs tracking-[0.08em] !leading-5">
+          {desc}
+        </p>
+      </div>
+      <div class="space-y-2 mt-8">
+        {#if repo}
+          <Button
+            icon="uil:github"
+            text="Source Code"
+            variant="outlined"
+            href={repo}
+            target="_blank"
+          />
+          {#if url}
+            <Button
+              icon="uil:arrow-right"
+              text="Live Demo"
+              iconPosition="end"
+              href={url}
+              target="_blank"
+            />
+          {/if}
+        {:else if url}
+          <Button
+            icon="uil:arrow-right"
+            text="Visit Site"
+            iconPosition="end"
+            href={url}
+            target="_blank"
+          />
+        {:else}
+          <Button
+            icon="uil:lock"
+            text="Private / Internal"
+            disabled
+            className="w-full"
+          />
+        {/if}
+      </div>
+    </div>
+  </div>
+</div>
+
+<style global>
+  .saos > div,
+  .saos > div > div {
+    height: 100%;
+  }
+</style>
